c46d82c2cc14b7c59224e4c63465c4e8b523db69
JalalMadhaji <jalal.almadhaji90@gmail.com>
2020-01-15 12:22:46 +0300

    create skills section

--- js/main.js
+++ js/main.js
@@ -6,5 +6,25 @@ $(document).ready(function() {
     } else {
       $(".menu").removeClass("fixed-menu");
     }
+
+    //get document scroll position
+    var position = $(document).scrollTop();
+    //get header height
+    var header = $(".menu").outerHeight();
+
+    //check active section
+    $(".section").each(function(i) {
+      $(".section .bar span")
+        .delay(500)
+        .queue(function() {
+          $(this).css("width", $(this).attr("data-power") + "%");
+        });
+      if ($(this).position().top <= position + header) {
+        $("a.active").removeClass("active");
+        $("a")
+          .eq(i)
+          .addClass("active");
+      }
+    });
   });
 });
