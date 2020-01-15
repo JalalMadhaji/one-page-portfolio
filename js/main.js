d236c4cc3f23ccc0afc0833d81ac7a65492cf84d
JalalMadhaji <jalal.almadhaji90@gmail.com>
2020-01-15 12:10:19 +0300

    create header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  // menu toggle class when scrolling
+  $(document).scroll(function() {
+    if ($(this).scrollTop() > 10) {
+      $(".menu").addClass("fixed-menu");
+    } else {
+      $(".menu").removeClass("fixed-menu");
+    }
+  });
+});
